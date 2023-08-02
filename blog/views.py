09ceb4790a09b806303b0ef7f6b7ec1dc1e9c212
KillerBee88 <hardmen08@gmail.com>
2023-08-02 18:30:29 +0300

    Update views.py
    show popular posts on main page.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
+from django.db.models import Count
 
 
 def get_related_posts_count(tag):
@@ -29,14 +30,14 @@ def serialize_tag(tag):
 
 def index(request):
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count')
 
-    fresh_posts = Post.objects.order_by('published_at')
-    most_fresh_posts = list(fresh_posts)[-5:]
+    fresh_posts = Post.objects.order_by('-published_at')[:5]
+    most_fresh_posts = list(fresh_posts)[:5]
 
     tags = Tag.objects.all()
-    popular_tags = sorted(tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    popular_tags = Tag.objects.annotate(posts_count=Count('posts')).order_by('-posts_count')
+    most_popular_tags = popular_tags[:5]
 
     context = {
         'most_popular_posts': [
