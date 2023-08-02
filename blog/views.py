7ca1028630581ff65d398879ef1ee1afb00d6f76
KillerBee88 <hardmen08@gmail.com>
2023-08-02 19:55:07 +0300

    Update views.py
    replace sorted func with order_by.

--- blog/views.py
+++ blog/views.py
@@ -37,11 +37,10 @@ def index(request):
     most_popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
 
     fresh_posts = Post.objects.order_by('published_at')
-    most_fresh_posts = list(fresh_posts)[-5:]
+    most_fresh_posts = list(fresh_posts)[:5]
 
-    tags = Tag.objects.all()
-    popular_tags = sorted(tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    popular_tags = Tag.objects.annotate(likes_count=Count('posts')).order_by('-likes_count')[:5]
+    most_popular_tags = popular_tags[:5]
 
     context = {
         'most_popular_posts': [
@@ -80,11 +79,10 @@ def post_detail(request, slug):
         'tags': [serialize_tag(tag) for tag in related_tags],
     }
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
+    popular_tags = Tag.objects.annotate(likes_count=Count('posts')).order_by('-likes_count')[:5]
     most_popular_tags = popular_tags[-5:]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
 
     context = {
         'post': serialized_post,
@@ -99,11 +97,10 @@ def post_detail(request, slug):
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
+    popular_tags = Tag.objects.annotate(likes_count=Count('posts')).order_by('-likes_count')[:5]
     most_popular_tags = popular_tags[-5:]
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count')[:5]
 
     related_posts = tag.posts.all()[:20]
 
